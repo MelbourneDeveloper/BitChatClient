7c06c557dc8332176280518597247c014feca058
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-08-06 12:49:03 +1000

    Chat model

---
+++ BitChatAppXamarinForms/BitChatAppXamarinForms.Standard/Model/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BitChatAppXamarinForms.Model
+{
+    public class Message : ModelBase
+    {
+        private string _Text;
+        private DateTime _MessageDate;
+
+        public string Text
+        {
+            get => _Text;
+            set
+            {
+                _Text = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DateTime MessageDate
+        {
+            get => _MessageDate; set
+            {
+                _MessageDate = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
---
+++ BitChatAppXamarinForms/BitChatAppXamarinForms.Standard/Model/Profile.cs
@@ -0,0 +1,27 @@
+﻿namespace BitChatAppXamarinForms.Model
+{
+    public class Profile : ModelBase
+    {
+        private string _ProfileName;
+        private string _EmailAddress;
+
+        public string ProfileName
+        {
+            get => _ProfileName;
+            set
+            {
+                _ProfileName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string EmailAddress
+        {
+            get => _EmailAddress; set
+            {
+                _EmailAddress = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
